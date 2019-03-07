dba1f50ae03e4886b2f20b6970c073ec1f24aa4d
josh-sea <joshua.cocciardi@gmail.com>
2019-03-07 13:28:09 -0500

    classroom changes

--- app/controllers/api/v1/classrooms_controller.rb
+++ app/controllers/api/v1/classrooms_controller.rb
@@ -9,12 +9,12 @@ class Api::V1::ClassroomsController < ApplicationController
   def create
     @classroom = Classroom.find_or_create_by(name: params[:name].downcase)
     if @classroom.users.find{|user|user.id === params[:user_id]}
-      ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: {}})
-      # render json: {classroom: @classroom, note: {}}, status: :ok
+      # ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: {}})
+      render json: {classroom: @classroom, note: {}}, status: :ok
     elsif !@classroom.users.find{|user|user.id === params[:user_id]}
       @note = Note.create(title: 'Untitled', content: '', user_id: params[:user_id], classroom_id: @classroom.id)
-      ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: @note})
-      # render json: {classroom: @classroom, note: @note}, status: :ok
+      # ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: @note})
+      render json: {classroom: @classroom, note: @note}, status: :ok
     end
   end
 
