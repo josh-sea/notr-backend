e90dbbb860beec7101c29c241ec88fa7d7d5c636
josh-sea <joshua.cocciardi@gmail.com>
2019-03-07 14:58:50 -0500

    classroom changes

--- app/controllers/api/v1/classrooms_controller.rb
+++ app/controllers/api/v1/classrooms_controller.rb
@@ -8,14 +8,22 @@ class Api::V1::ClassroomsController < ApplicationController
 
   def create
     @classroom = Classroom.find_or_create_by(name: params[:name].downcase)
-    if @classroom.users.find{|user|user.id === params[:user_id]}
-      # ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: {}})
-      render json: {classroom: @classroom, note: {}}, status: :ok
+    @user = @classroom.users.find{|user|user.id === params[:user_id]}
+    if @user
+      # ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: {}, user: @user})
+      render json: {classroom: @classroom, note: {}, user: @user}, status: :ok
     elsif !@classroom.users.find{|user|user.id === params[:user_id]}
       @note = Note.create(title: 'Untitled', content: '', user_id: params[:user_id], classroom_id: @classroom.id)
-      # ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: @note})
-      render json: {classroom: @classroom, note: @note}, status: :ok
+      # ActionCable.server.broadcast('new_classroom', {classroom: @classroom, note: @note, user: @user}))
+      render json: {classroom: @classroom, note: @note, user: @user}, status: :ok
     end
+  end
+
+  def show
+    @classroom = Classroom.find_by(id: params[:id])
+    @notes = @classroom.notes
+    @users = @classroom.users
+    render json: {classroom: @classroom, notes: @notes, users: @users}, status: :ok
   end
 
   private
